2d72f8479c0ccde37d33dc2b3507e2c74a181795
Pratham-Makwana <makwanapratham13@gmail.com>
2025-01-08 22:01:39 +0530

    WebSocket Chat Using Node

---
+++ Chat-App/index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+    // console.log("A new messgage", message);
+  });
+  //   console.log("new user", socket.id);
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("./public/index.html");
+});
+
+server.listen(8000, () => console.log(`Server Listen at 8000`));
